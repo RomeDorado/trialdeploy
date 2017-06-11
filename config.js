b73aa37d4ad3b19fc1576dabefd20e4be5e07ea6
Rome Dorado <romedorado@gmail.com>
2017-06-11 15:28:07 +0800

    rome did this

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
+    FB_PAGE_TOKEN: 'EAAa7KyJTDv0BAP5dXQO8XzN1lZACyZCQGVhhSxZBzOo2rbkRElJr12M2sfOLLITDpekv883t6CUY7xx0ZAOXFQ3CxhGWhuqaevh4cjo6IxHqAs9uQa0V1uiXZAEz5R0XQm3ibqQMUAe5ETzKeYlVYSYxhZAxhpV4Lsg4tZBdi9ntAZDZD',
+    FB_VERIFY_TOKEN: 'samplehonestbee',
     API_AI_CLIENT_ACCESS_TOKEN: '',
     FB_APP_SECRET: '',
-    SERVER_URL: "",
+    SERVER_URL: "https://honestbeebot.herokuapp.com/",
 };
