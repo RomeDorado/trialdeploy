b990d65a8ff03a4908503a67b794d6e3b7f3d979
Rome Dorado <romedorado@gmail.com>
2017-06-12 14:06:09 +0800

    addd

--- app.js
+++ app.js
@@ -308,6 +308,7 @@ function handleApiAiResponse(sender, response) {
 		let previousType ;
 		let cardTypes = [];
 		let timeout = 0;
+		handleApiAiAction(sender, action, responseText, contexts, parameters);
 		for (var i = 0; i < messages.length; i++) {
 
 			if ( previousType == 1 && (messages[i].type != 1 || i == messages.length - 1)) {
