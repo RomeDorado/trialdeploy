f9ff225b664343f66771c2347c0e4d72f80c0148
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-12 20:14:25 +0800

    edited verify

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     FB_PAGE_TOKEN: 'EAAa7KyJTDv0BAP5dXQO8XzN1lZACyZCQGVhhSxZBzOo2rbkRElJr12M2sfOLLITDpekv883t6CUY7xx0ZAOXFQ3CxhGWhuqaevh4cjo6IxHqAs9uQa0V1uiXZAEz5R0XQm3ibqQMUAe5ETzKeYlVYSYxhZAxhpV4Lsg4tZBdi9ntAZDZD',
-    FB_VERIFY_TOKEN: 'samplehonestbee',
+    FB_VERIFY_TOKEN: 'honestbeesample',
     API_AI_CLIENT_ACCESS_TOKEN: '181cf58d1ebf4665b25e62ea284f8560',
     FB_APP_SECRET: '7e3da246d5eb37ec54bfaf79afb068e8',
     SERVER_URL: "https://honestbeebot.herokuapp.com/",
