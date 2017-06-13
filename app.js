6db87be896cc5bc0c4e0de477d75a0ab1eee6c89
Rome Dorado <romedorado@gmail.com>
2017-06-13 21:15:31 +0800

    getstarted

--- app.js
+++ app.js
@@ -724,7 +724,7 @@ function greetUserText(userId) {
 				console.log("FB user: %s %s, %s",
 					user.first_name, user.last_name, user.gender);
 
-				sendTextMessage(userId, "Hi! " + user.first_name + 'I\'m HonestBee bot, your one-stop platform for an easier, more productive life 🐝  To continue, are you an HonestBee consumer or are you an HonestBee merchant?');
+				sendTextMessage(userId, "Hi " + user.first_name + '! I\'m HonestBee bot, your one-stop platform for an easier, more productive life 🐝  To continue, are you an HonestBee consumer or are you an HonestBee merchant?');
 			} else {
 				console.log("Cannot get data for fb user with id",
 					userId);
