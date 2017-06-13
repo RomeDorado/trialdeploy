acb796fda34aaf76a01f0e1db9c023bfdd1b86e9
Rome Dorado <romedorado@gmail.com>
2017-06-13 13:48:59 +0800

    counter

--- app.js
+++ app.js
@@ -211,44 +211,9 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 		 	//sendTextMessage(sender, responseText);
 		 break;
 		 
-		 case "says-hi":		 
-	request({
-		uri: 'https://graph.facebook.com/v2.7/' + userId,
-		qs: {
-			access_token: config.FB_PAGE_TOKEN
-		}
-
-	}, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-
-			var user = JSON.parse(body);
-
-			if (user.first_name) {
-				console.log("FB user: %s %s, %s",
-					user.first_name, user.last_name, user.gender);
-
-				var messageData = {
-		recipient: {
-			id: recipientId
-		},
-		message: {
-			text: user.firstname
-		}
-	}
-	callSendAPI(messageData);
-				sendTextMessage(userId, "Welcome " + user.first_name + '!');
-			} else {
-				console.log("Cannot get data for fb user with id",
-					userId);
-			}
-		} else {
-			console.error(response.error);
-		}
-
-	});	
-
-
-
+		 case "says-hi":
+		 function greetUserText(userId)
+	
 		 break;
 		 
 		default:
