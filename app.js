4cea039c4481adb7dcffe6e282641ca0a42c0e77
Rome Dorado <romedorado@gmail.com>
2017-06-14 12:11:39 +0800

    greet user with quick replies

--- app.js
+++ app.js
@@ -226,48 +226,6 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 	}
 }
 
-var hMessage = function handleMessage(message, sender) {
-
-	switch (message.type) {
-
-		case 0: //text
-
-			sendTextMessage(sender, message.speech);
-			console.log("handle message napupunta");
-		break;
-
-		case 2: //quick replies
-			let replies = [];
-			for (var b = 0; b < message.replies.length; b++) {
-				let reply =
-				{
-					"content_type": "text",
-					"title": message.replies[b],
-					"payload": message.replies[b]
-				}
-				replies.push(reply);
-			}
-			sendQuickReply(sender, message.title, replies);
-			break;
-		case 3: //image
-			sendImageMessage(sender, message.imageUrl);
-			break;
-		case 4:
-			// custom payload
-			var messageData = {
-				recipient: {
-					id: sender
-				},
-				message: message.payload.facebook
-
-			};
-
-			callSendAPI(messageData);
-			console.log("PAYLOAD LOG");
-			break;
-	}
-}
-
 
 function handleCardMessages(messages, sender) {
 
