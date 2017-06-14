d8347b25c3ab26a5b4f91e06c8185cd15e019fa9
Rome Dorado <romedorado@gmail.com>
2017-06-14 12:03:12 +0800

    gg

--- app.js
+++ app.js
@@ -317,7 +317,7 @@ function handleApiAiResponse(sender, response) {
 
 	sendTypingOff(sender);
 		
-	if (isDefined(messages) && (messages.length == 1 && messages[0].type != 0 || messages.length > 1) && action != "input.unknown"){
+	if (isDefined(messages) && (messages.length == 1 && messages[0].type != 0 || messages.length > 1) && action != "input.unknown" && action != "feedback-action"){
 		let timeoutInterval = 1100;
 		let previousType ;
 		let cardTypes = [];
