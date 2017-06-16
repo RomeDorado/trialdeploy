557337bdfaa6e09a76db3d716775267ee2f40e3b
Patrick Ian M. Co <patrickianco@outlook.com>
2017-06-16 11:31:20 +0800

    feedback

--- app.js
+++ app.js
@@ -355,6 +355,7 @@ function handleApiAiResponse(sender, response) {
 		sendTextMessage(sender, "I'm not sure what you want. Can you be more specific?");
 	} else if (isDefined(action)) {
 		console.log('this is the action' + action);
+		console.log(responseText);
 		handleApiAiAction(sender, action, responseText, contexts, parameters);
 	} else if (isDefined(responseData) && isDefined(responseData.facebook)) {
 		try {
